e262fce699fb93a1695878449f2281f011fcb009
Marc Haemmerle <mail@marchaemmerle.com>
2014-03-29 13:08:59 +0100

    add enemy system
    leaner repository creation

--- src/clj/entitas_clj/example.clj
+++ src/clj/entitas_clj/example.clj
@@ -13,6 +13,12 @@
         position (c/create ctype {:x x :y y :char "X"})]
     (e/add-component (e/create entity-id) position)))
 
+(defn create-enemy [x y]
+  (let [entity-id :enemy
+        ctype :position
+        position (c/create ctype {:x x :y y :char "E"})]
+    (e/add-component (e/create entity-id) position)))
+
 (defn handle-input [{:keys [x y] :as position} input]
   (let [[new-x new-y] (case input
                         :left [(- x 1) y]
@@ -25,6 +31,18 @@
 (defn update-position [entity input]
   (let [component (e/component-of-type entity :position)]
     (update-in component [:data] handle-input input)))
+
+(defn create-input-emitter [screen]
+
+
+  )
+
+(defn execute-enemy-move-system [repository]
+  (let [[new-repository rc] (r/collection-for-types repository #{:enemy})]
+    (println "move enemy")
+    repository
+    )
+  )
 
 ;; screen should be in render component
 (defn execute-render-system [screen repository]
@@ -39,17 +57,27 @@
 
 (defn start [width height]
   (let [screen (ls/get-screen :swing {:cols width :rows height})
+        ;; player
         player0 (create-player 0 0)
         player (e/add-component player0 (c/create :render nil))
-        repository (r/add-entity (r/create) player)
-        render-system (s/create :render #(execute-render-system screen %))]
+        ;; enemy
+        enemy0 (create-enemy 22 22)
+        enemy (e/add-component enemy0 (c/create :render nil))
+        ;; add
+        repository (-> (r/create)
+                       (r/add-entity ,, player)
+                       (r/add-entity ,, enemy))
+        ;; render system
+        render-system (s/create :render-system #(execute-render-system screen %))
+        enemy-system (s/create :enemy-system execute-enemy-move-system)
+        systems [render-system enemy-system]]
     (ls/start screen)
     (loop [input (ls/get-key-blocking screen)
-           state {:systems [render-system] :repository repository}]
+           state {:systems systems :repository repository}]
       (let [position-component (update-position player input)
             repository0 (cr/exchange-component (:repository state) player position-component)
             repository1 (s/execute (:systems state) repository0)]
         (recur (ls/get-key-blocking screen)
                (assoc state :repository repository1))))))
 
-;; (start 40 40)
+(start 40 40)
